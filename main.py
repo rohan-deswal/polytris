11f8c037d4ebacb3eaad753e371353de4fdbcba7
Rohan Deswal <mail.rohandeswal@gmail.com>
2021-04-07 22:45:08 +0530

    Revert "Made the GameBoard class"

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 import pyglet
-from gameBoard import *
+from cell import *
 
-n = 4 #the 'n' in n-tris
 
-width = 600
-height = 600
+grid = 30
+window = pyglet.window.Window(20*grid,20*grid,caption = 'Tetris')
 
-window = pyglet.window.Window(width, height, caption = 'Tetris')
-batch = pyglet.graphics.Batch()
-
-# cell = Cell([window.width//2, window.height//2], grid, (255,0,0))
-
-gameBoard = GameBoard((0, 0), (width, height), 4)
-
-# def draw_label(text, x, y, font_size):
-# 	label = pyglet.text.Label("Hello, World!!",
-# 					   font_name="Times New Roman",
-# 					   font_size=font_size,
-# 					   x=x, y=y, 
-# 					   anchor_x='center', 
-# 					   anchor_y='center')
-# 	label.draw()
+cell = Cell([window.width//2, window.height//2], grid, (255,0,0))
+def draw_label(text, x, y, font_size):
+	label = pyglet.text.Label("Hello, World!!",
+					   font_name="Times New Roman",
+					   font_size=font_size,
+					   x=x, y=y, 
+					   anchor_x='center', 
+					   anchor_y='center')
+	label.draw()
 
 @window.event
 def on_draw():
 	pyglet.gl.glClearColor(0,0,0,1)
 	window.clear()
-	gameBoard.draw_hold(lambda x, y, b:None)
-	gameBoard.draw_playing_area(lambda x, y, b:None)
-	gameBoard.draw_preview(lambda x, y, b:None)
 
+	pyglet.shapes.Rectangle(grid*4 + grid//2, 0, 10*grid, 20*grid, (255,255,255)).draw()
 
-# pyglet.clock.schedule(update)
+	cell.draw()
+
+@window.event
+def update(dt):
+	cell.update(dt, 0, -grid)
+
+pyglet.clock.schedule(update)
 pyglet.app.run()
