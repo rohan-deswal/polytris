e5117c984e37009ba262259e9778cd265565e6ee
Mudit Aggarwal <13mudit@gmail.com>
2021-04-07 22:13:00 +0530

    Made the GameBoard class which handles the drawing of playing area and more

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 import pyglet
-from cell import *
+from gameBoard import *
 
+n = 4 #the 'n' in n-tris
 
-grid = 30
-window = pyglet.window.Window(20*grid,20*grid,caption = 'Tetris')
+width = 600
+height = 600
 
-cell = Cell([window.width//2, window.height//2], grid, (255,0,0))
-def draw_label(text, x, y, font_size):
-	label = pyglet.text.Label("Hello, World!!",
-					   font_name="Times New Roman",
-					   font_size=font_size,
-					   x=x, y=y, 
-					   anchor_x='center', 
-					   anchor_y='center')
-	label.draw()
+window = pyglet.window.Window(width, height, caption = 'Tetris')
+batch = pyglet.graphics.Batch()
+
+# cell = Cell([window.width//2, window.height//2], grid, (255,0,0))
+
+gameBoard = GameBoard((0, 0), (width, height), 4)
+
+# def draw_label(text, x, y, font_size):
+# 	label = pyglet.text.Label("Hello, World!!",
+# 					   font_name="Times New Roman",
+# 					   font_size=font_size,
+# 					   x=x, y=y, 
+# 					   anchor_x='center', 
+# 					   anchor_y='center')
+# 	label.draw()
 
 @window.event
 def on_draw():
 	pyglet.gl.glClearColor(0,0,0,1)
 	window.clear()
+	gameBoard.draw_hold(lambda x, y, b:None)
+	gameBoard.draw_playing_area(lambda x, y, b:None)
+	gameBoard.draw_preview(lambda x, y, b:None)
 
-	pyglet.shapes.Rectangle(grid*4 + grid//2, 0, 10*grid, 20*grid, (255,255,255)).draw()
 
-	cell.draw()
-
-@window.event
-def update(dt):
-	cell.update(dt, 0, -grid)
-
-pyglet.clock.schedule(update)
+# pyglet.clock.schedule(update)
 pyglet.app.run()
