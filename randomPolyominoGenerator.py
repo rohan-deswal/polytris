686e86259103b0e913a6188cc7e138ffc84a59ed
rohan-deswal <mail.rohandeswal@gmail.com>
2021-04-09 20:07:46 +0530

    Polyomino Generator Modified

--- randomPolyominoGenerator.py
+++ randomPolyominoGenerator.py
@@ -1,26 +1,26 @@
-import numpy as np
-
+from random import shuffle
 def getRandomPolyomino(n):
 	# 1-n Polyomino
-	polyominoBase = np.array([[0,0]])
+	polyominoBase = ([[0,0]])
 	piecesAdded = 1
 	for piece in polyominoBase:
 		x = piece[0]
 		y = piece[1]
-
+		# print('x = '+str(x),'y = '+str(y))
 		for i in range(-1,2):
 			for j in range(-1,2):
-				if i != 0 or j != 0:
-					newX = x-i
-					newY = y-j
-
-					if not ([newX,newY] in polyominoBase) and any([ele in polyominoBase for ele in np.array([[newX-1,newY],[newX+1,newY],[newX,newY+1],[newX,newY-1]])]):
-						polyominoBase = np.append(polyominoBase,[newX,newY])
-						np.random.shuffle(polyominoBase)
-						piecesAdded += 1
-						if piecesAdded == n:
-							print(piecesAdded)
-							return polyominoBase
+				if i**2 + j**2 != 0:
+					newX = x+i
+					newY = y+j
+					# print('newX = '+str(newX),'newY = '+str(newY))
+					if [newX,newY] not in polyominoBase:
+						if [newX-1,newY] in polyominoBase or [newX+1,newY] in polyominoBase or [newX,newY-1] in polyominoBase or [newX,newY+1] in polyominoBase:
+							polyominoBase.append([newX,newY])
+							# print(polyominoBase)
+							shuffle(polyominoBase)
+							piecesAdded += 1
+							if piecesAdded == n:
+								return polyominoBase
 
 
 print(getRandomPolyomino(4))
