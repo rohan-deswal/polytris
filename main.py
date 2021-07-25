b1155fd5f71b9ceffb215ccba59cc90f4b99566d
Mudit Aggarwal <13mudit@gmail.com>
2021-07-26 02:26:24 +0530

    implemented hand recognition Still requires gesture recognition Currently program just prints hand coordinates onto terminal using multi processing

--- main.py
+++ main.py
@@ -3,37 +3,58 @@ from gameBoard import *
 from pyglet.window.key import *
 from time import time
 
-n = 4 #the 'n' in n-tris
-width = 960
-height = 600
+from multiprocessing import Process, Queue
 
-board_size = (20, 20)
+from mediapipe_hands import *
 
-window = pyglet.window.Window(width, height, caption = 'Polytris')
-batch = pyglet.graphics.Batch()
-keys = KeyStateHandler()
+def main():
+	n = 1 #the 'n' in n-tris
+	width = 960
+	height = 600
 
-gameBoard = GameBoard((0, 0), (width, height), n, board_size)
+	board_size = (20, 20)
 
-@window.event
-def on_key_press(symbol,modifiers):
-	gameBoard.event_handler(symbol, modifiers)
+	window = pyglet.window.Window(width, height, caption = 'Polytris')
+	batch = pyglet.graphics.Batch()
+	keys = KeyStateHandler()
 
-@window.event
-def on_key_release(symbol,modifiers):
-	gameBoard.event_handler(symbol, modifiers, key_release=True)
+	gameBoard = GameBoard((0, 0), (width, height), n, board_size)
 
-@window.event
-def on_draw():
-	pyglet.gl.glClearColor(0,0,0,1)
-	window.clear()
-	gameBoard.draw_hold()
-	gameBoard.draw_playing_area()
-	gameBoard.draw_preview()
+	@window.event
+	def on_key_press(symbol,modifiers):
+		gameBoard.event_handler(symbol, modifiers)
 
-@window.event
-def update(dt):
-	gameBoard.update(window, keys)
+	@window.event
+	def on_key_release(symbol,modifiers):
+		gameBoard.event_handler(symbol, modifiers, key_release=True)
 
-pyglet.clock.schedule(update)
-pyglet.app.run()
+	@window.event
+	def on_draw():
+		pyglet.gl.glClearColor(0,0,0,1)
+		window.clear()
+		gameBoard.draw_hold()
+		gameBoard.draw_playing_area()
+		gameBoard.draw_preview()
+
+	@window.event
+	def update(dt):
+		gameBoard.update(window, keys)
+
+	pyglet.clock.schedule(update)
+	pyglet.app.run()
+
+def test_main(queue):
+	while True:
+		start = time()
+		print(queue.get())
+		print("Elapsed:", time() - start)
+
+if __name__ == "__main__":
+	# main()
+	q = Queue()
+	p1 = Process(target=hand_capture, args=(q,))
+	p2 = Process(target=test_main, args=(q,))
+	p1.start()
+	p2.start()
+	p1.join()
+	p2.join()
---
+++ mediapipe_hands.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def hand_capture(queue):
+    #multiprocessing queue for communication between processes    
+    import cv2
+    import mediapipe as mp
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
+
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    with mp_hands.Hands(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.2,
+        max_num_hands=1) as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            # Flip the image horizontally for a later selfie-view display, and convert
+            # the BGR image to RGB.
+            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            results = hands.process(image)
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                coordinates = np.array([[landmark.x, landmark.y, landmark.z] for landmark in results.multi_hand_landmarks[0].landmark], dtype=np.float16)
+                queue.put(coordinates)
+                # for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(image, results.multi_hand_landmarks[0], mp_hands.HAND_CONNECTIONS)
+            cv2.imshow('MediaPipe Hands', image)
+            if cv2.waitKey(20) & 0xFF == ord('d'):
+                break
+
+    cap.release()
